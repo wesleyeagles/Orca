762a677843c40d4513939b37fa42097749ffb5c0
Wesley Carvalho <crafael.wesley@gmail.com>
2022-06-09 12:27:06 -0300

    First Commit

---
+++ src/routes/index.tsx
@@ -0,0 +1,14 @@
+import { Routes, Route, Navigate } from "react-router-dom";
+
+export const AppRoutes = () => {
+
+    return (
+        <Routes>
+            <Route path="/" element={<p>Página inicial</p>} />
+
+            <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+    );
+
+}
+
---
+++ src/shared/themes/index.ts
@@ -0,0 +1,13 @@
+import { createTheme } from '@mui/material'
+
+export const LightTheme = createTheme({
+    palette: {
+        primary: {
+            main: '#4C83FF',
+            dark: '#153C94',
+            light: '#80A7FF',
+            contrastText: '#FFFFFF',
+        }
+    }
+});
+
